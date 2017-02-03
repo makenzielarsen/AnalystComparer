fb84f303175f21eef12b6f8051adabfb64ff528c
makenzielarsen <makenzie97larsen@gmail.com>
2017-02-02 20:22:38 -0700

    Adds Trade header

---
+++ AnalystComparer/Trade.h
@@ -0,0 +1,38 @@
+//
+// Created by Makenzie Larsen on 2/2/17.
+//
+
+#ifndef ANALYSTCOMPARER_TRADE_H
+#define ANALYSTCOMPARER_TRADE_H
+
+
+#include <string>
+
+class Trade {
+private:
+    std::string stockSymbol;
+    double quantity;
+    double purchaseDateAndTime;
+    double purchasePrice;
+    double purchaseTransactionFee;
+    double saleDateAndTime;
+    double salePrice;
+    double saleTransactionFee;
+    double moneyInvested = {quantity * purchasePrice + purchaseTransactionFee + saleTransactionFee};
+    double profit = {quantity * salePrice - moneyInvested};
+public:
+    int load(std::ifstream& inputStream);
+    std::string getStockSymbol() { return stockSymbol; }
+    double getQuantity() { return quantity; }
+    double getPurchasePrice() { return purchasePrice; }
+    double getPurchaseDateAndTime() { return purchaseDateAndTime; }
+    double getPurchaseTransactionFee() { return purchaseTransactionFee; }
+    double getSaleDateAndTime() { return saleDateAndTime; }
+    double getSalePrice() { return salePrice; }
+    double getSaleTransactionFee() { return saleTransactionFee; }
+    double getMoneyInvested() { return moneyInvested };
+    double getProfit() { return profit; }
+
+};
+
+#endif //ANALYSTCOMPARER_TRADE_H
