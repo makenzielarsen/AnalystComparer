01c4f78825e4b54c7beb3783ca8e10dd8b6680d4
makenzielarsen <makenzie97larsen@gmail.com>
2017-02-02 20:26:30 -0700

    Implements load function

---
+++ AnalystComparer/Trade.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Makenzie Larsen on 2/2/17.
+//
+
+#include <fstream>
+#include "Trade.h"
+
+int Trade::load(std::ifstream &inputStream) {
+    inputStream >> stockSymbol;
+    inputStream >> quantity;
+    inputStream >> purchaseDateAndTime;
+    inputStream >> purchasePrice;
+    inputStream >> purchaseTransactionFee;
+    inputStream >> saleDateAndTime;
+    inputStream >> salePrice;
+    inputStream >> saleTransactionFee;
+}
